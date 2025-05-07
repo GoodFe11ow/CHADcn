98a0fe99ccc73825dab0228e477dde00686f7807
Mikhail <lalkazatralen2@gmail.com>
2025-05-07 10:58:54 +0300

    views router commit

---
+++ chad/src/router/index.js
@@ -0,0 +1,14 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import Home from '../Views/Home.vue';
+import About from '../Views/About.vue';
+import Contact from '../Views/Contact.vue';
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/contact', component: Contact },
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
