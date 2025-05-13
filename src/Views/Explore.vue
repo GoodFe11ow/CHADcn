5ac5ca65848913e8461743005336654a3387f56b
Sorel-Haller <hallersorel@gmail.com>
2025-05-13 18:43:16 +0300

    Added pagination, sheet, badge components

--- src/Views/Explore.vue
+++ src/Views/Explore.vue
@@ -1,10 +1,12 @@
 <template>
   <exploreHeading />
   <exploreNav />
+  <imageCard />
   <Footer />
 </template>
 
 <script setup>
 import exploreHeading from '@/components/exploreHeading.vue';
 import exploreNav from '@/components/exploreNav.vue';
+import imageCard from '@/components/imageCard.vue';
 </script>
