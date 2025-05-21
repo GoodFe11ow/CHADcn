b60c29d64c31b94a12536df9de7a4148b9644bce
Sorel-Haller <hallersorel@gmail.com>
2025-05-21 23:29:12 +0300

    Added favoritesHeading to favorites.vue + added text and buttons to it

--- src/Views/Favorites.vue
+++ src/Views/Favorites.vue
@@ -1,8 +1,10 @@
 <template>
   <favoriteSubheader />
+  <FavoritesHeading />
   <footer />
 </template>
 
 <script setup>
+import FavoritesHeading from '@/components/favoritesHeading.vue';
 import favoriteSubheader from '@/components/favoritesSubheader.vue';
 </script>
