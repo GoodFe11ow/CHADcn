50d43c81fdc82ff3438b9852df569d3e17dc3d10
Mikhail <lalkazatralen2@gmail.com>
2025-05-25 21:22:10 +0300

    made bankcard campaign

---
+++ src/components/MyCampaignsBankCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-[430px] h-[330px]">
+        <div class="flex overflow-hidden  rounded-[27px] relative">
+            <div class="grow bg-lime-300 pt-[32px] pl-[21px]">
+                <div class="pb-[48px] pl-[6px]">
+                    <p class="font-semibold text-sm">Crowdify</p>
+                    <h4 class="font-semibold text-[27px]/8 w-[200px]">Renovate Kingstown Orphanage</h4>
+                </div>
+                <div class="pb-[24px]">
+                    <p class="uppercase text-[10px]">ana yamamoto</p>
+                    <p class="font-semibold">5226 6749 6789 4341</p>
+                </div>
+            </div>
+            <img src="/src/assets/cardIcon.svg" alt="" class="absolute bottom-4 right-5">
+            <img src="/src/assets/crowdfyIcon.svg" alt="" class="absolute top-8 right-5">
+            <img src="/src/assets/bankcardimg.png" class="object-center object-cover w-[108px] h-[100]" alt="">
+        </div>
+        <div class="flex justify-between mt-5 mb-2">
+            <p>Total donations</p>
+            <p class="font-semibold text-[19px] text-gray-600">$2,840.40</p>
+        </div>
+        <Progress model-value="55" />
+    </div>
+</template>
+
+<script setup>
+    import { Progress } from './ui/progress';
+</script>
--- src/components/MyCampaignsMain.vue
+++ src/components/MyCampaignsMain.vue
@@ -1,7 +1,26 @@
 <template>
     <myCampagnSubheader />
+    <div>
+        <div>
+            <div class=" border-1 border-gray-200">
+                <MyCampaignsNav />
+                <div class="flex flex-wrap gap-5">
+                    <MyCampaignsBankCard />
+                    <MyCampaignsBankCard />
+                    <MyCampaignsBankCard />
+                    <MyCampaignsBankCard />
+                </div>
+            </div>
+            <div></div>
+        </div>
+        <div>
+
+        </div>
+    </div>
 </template>
 
 <script setup>
+    import MyCampaignsBankCard from './MyCampaignsBankCard.vue';
+    import MyCampaignsNav from './MyCampaignsNav.vue';
     import myCampagnSubheader from './myCampagnSubheader.vue';
 </script>
